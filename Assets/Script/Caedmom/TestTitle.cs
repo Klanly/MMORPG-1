99ff3bf5cbd4039b545bac26d9990b30d1562b23
caedmom <caedmom.chen@foxmail.com>
2018-11-28 16:50:15 +0800

    初始化/Initialization
    创建工程，导入相关包/Create Project,Import packages

---
+++ Assets/Script/Caedmom/TestTitle.cs
@@ -0,0 +1,30 @@
+#region 模块信息
+// **********************************************************************
+// Copyright (C) 2018 The company name
+//
+// 文件名(File Name):             TestTitle.cs
+// 作者(Author):                  Caedmom
+// 创建时间(CreateTime):          2018/11/27 12:27:15
+// 修改者列表(modifier):
+// 模块描述(Module description):
+// **********************************************************************
+#endregion
+using UnityEngine;
+using System.Collections;
+
+public class TestTitle : MonoBehaviour {
+
+	// Awake
+	void Awake () {
+	
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
