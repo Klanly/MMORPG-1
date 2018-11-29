2d5eec8aae37773cb09d03b1382e7d2144884296
caedmom <caedmom.chen@foxmail.com>
2018-11-29 11:27:45 +0800

    .

--- Assets/Script/Caedmom/MMO_MemoryStream.cs
+++ Assets/Script/Caedmom/MMO_MemoryStream.cs
@@ -13,6 +13,7 @@ using UnityEngine;
 using System.Collections;
 using System.IO;
 using System;
+using System.Text;
 
 public class MMO_MemoryStream : MemoryStream {
     #region Short
@@ -40,6 +41,10 @@ public class MMO_MemoryStream : MemoryStream {
     #endregion
   
     #region UShort
+    /// <summary>
+    /// 从流中读取一个UShort数据
+    /// </summary>
+    /// <returns></returns>
     public ushort ReadUShort()
     {
         byte[] arr = new byte[2];
@@ -47,6 +52,10 @@ public class MMO_MemoryStream : MemoryStream {
         return BitConverter.ToUInt16(arr, 0);
     }
     
+    /// <summary>
+    /// 将一个UShort数据写入流
+    /// </summary>
+    /// <param name="value"></param>
     public void WriteUShort(ushort value)
     {
         byte[] arr = BitConverter.GetBytes(value);
@@ -55,13 +64,20 @@ public class MMO_MemoryStream : MemoryStream {
     #endregion
 
     #region Int
+    /// <summary>
+    /// 从流中读取一个Int数据
+    /// </summary>
+    /// <returns></returns>
     public int ReadInt()
     {
         byte[] arr = new byte[4];
         base.Read(arr, 0, 4);
         return BitConverter.ToInt32(arr, 0);
     }
-
+    /// <summary>
+    /// 将一个Int数据写入流
+    /// </summary>
+    /// <param name="value"></param>
     public void WriteInt(int value)
     {
         byte[] arr = BitConverter.GetBytes(value);
@@ -70,13 +86,20 @@ public class MMO_MemoryStream : MemoryStream {
     #endregion
 
     #region UInt
+    /// <summary>
+    /// 从流中读取一个UInt数据
+    /// </summary>
+    /// <returns></returns>
     public uint ReadUInt()
     {
         byte[] arr = new byte[4];
         base.Read(arr, 0, 4);
         return BitConverter.ToUInt32(arr, 0);
     }
-
+    /// <summary>
+    /// 将一个UInt数据写入流
+    /// </summary>
+    /// <param name="value"></param>
     public void Write(uint value)
     {
         byte[] arr = BitConverter.GetBytes(value);
@@ -85,13 +108,20 @@ public class MMO_MemoryStream : MemoryStream {
     #endregion
 
     #region Long
+    /// <summary>
+    /// 从流中读取一个Long数据
+    /// </summary>
+    /// <returns></returns>
     public long ReadLong()
     {
         byte[] arr = new byte[8];
         base.Read(arr, 0, 8);
         return BitConverter.ToInt64(arr, 0);
     }
-
+    /// <summary>
+    /// 将一个Long数据写入流
+    /// </summary>
+    /// <param name="value"></param>
     public void WriteLong(long value)
     {
         byte[] arr = BitConverter.GetBytes(value);
@@ -100,13 +130,20 @@ public class MMO_MemoryStream : MemoryStream {
     #endregion
 
     #region ULong
+    /// <summary>
+    /// 从流中读取一个ULong数据
+    /// </summary>
+    /// <returns></returns>
     public ulong ReadULong()
     {
         byte[] arr = new byte[8];
         base.Read(arr, 0, 8);
         return BitConverter.ToUInt64(arr, 0);
     }
-
+    /// <summary>
+    /// 将一个ULong数据写入流
+    /// </summary>
+    /// <param name="value"></param>
     public void WriteULong(ulong value)
     {
         byte[] arr = BitConverter.GetBytes(value);
@@ -114,5 +151,94 @@ public class MMO_MemoryStream : MemoryStream {
     }
     #endregion
 
+    #region Float
+    /// <summary>
+    /// 从流中读取一个float数据
+    /// </summary>
+    /// <returns></returns>
+    public float ReadFloat()
+    {
+        byte[] arr = new byte[4];
+        base.Read(arr, 0, 4);
+        return BitConverter.ToSingle(arr, 0);
+    }
+    /// <summary>
+    /// 将一个float数据写入流
+    /// </summary>
+    /// <param name="value"></param>
+    public void Write(float value)
+    {
+        byte[] arr = BitConverter.GetBytes(value);
+        base.Write(arr, 0, arr.Length);
+    }
+    #endregion
 
+    #region Double
+    /// <summary>
+    /// 从流中读取一个Double数据
+    /// </summary>
+    /// <returns></returns>
+    public double ReadDouble()
+    {
+        byte[] arr = new byte[8];
+        base.Read(arr, 0, 8);
+        return BitConverter.ToDouble(arr, 0);
+    }
+    /// <summary>
+    /// 将一个double数据写入流
+    /// </summary>
+    /// <param name="value"></param>
+    public void WriteDouble(double value)
+    {
+        byte[] arr = BitConverter.GetBytes(value);
+        base.Write(arr, 0, arr.Length);
+    }
+    #endregion
+
+    #region Bool
+    /// <summary>
+    /// 从流中读取一个bool数据
+    /// </summary>
+    /// <returns></returns>
+    public bool ReadBool()
+    {
+        return base.ReadByte() == 1;
+    }
+    /// <summary>
+    /// 把一个bool数据写入流
+    /// </summary>
+    /// <param name="value"></param>
+    public void WriteBool(bool value)
+    {
+        base.WriteByte((byte)(value == true ? 1 : 0));
+    }
+    #endregion
+
+    #region UTF8String
+    /// <summary>
+    /// 从流中读取一个string数组
+    /// </summary>
+    /// <returns></returns>
+    public string ReadUTF8String()
+    {
+        ushort len = this.ReadUShort();
+        byte[] arr = new byte[len];
+        base.Read(arr, 0, len);
+        return Encoding.UTF8.GetString(arr);
+    }
+    /// <summary>
+    /// 把一个string数据写入流
+    /// </summary>
+    /// <param name="str"></param>
+    public void WriteUTF8String(string str)
+    {
+        byte[] arr = Encoding.UTF8.GetBytes(str);
+        if (arr.Length > 65535)
+        {
+            throw new InvalidCastException("字符串长度超出范围");
+        }
+        WriteUShort((ushort)arr.Length);
+        base.Write(arr, 0, arr.Length);
+    }
+    #endregion
 }
---
+++ Assets/Script/Caedmom/TestMMOMemory.cs
@@ -0,0 +1,30 @@
+#region 模块信息
+// **********************************************************************
+// Copyright (C) 2018 The company name
+//
+// 文件名(File Name):             TestMMOMemory.cs
+// 作者(Author):                  Caedmom
+// 创建时间(CreateTime):          2018/11/29 11:22:43
+// 修改者列表(modifier):
+// 模块描述(Module description):
+// **********************************************************************
+#endregion
+using UnityEngine;
+using System.Collections;
+
+public class TestMMOMemory : MonoBehaviour {
+
+	// Awake
+	void Awake () {
+	
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
